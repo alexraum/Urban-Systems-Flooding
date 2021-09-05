9f8bb9cb689c05290a52c161b159854fd6311b8f
maraum <maraum@ncsu.edu>
2021-09-05 02:59:42 -0400

    Initial Commit

---
+++ src/ControlPredInvst.py
@@ -0,0 +1,82 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Mar  5 00:25:55 2021
+
+@author: alex raum
+"""
+
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+
+# define macros
+HEIGHT = 10
+WIDTH = 14
+MONTHS = 48
+NUM_OBS = 20
+POST_MATT = 23
+POST_FLOR = 48
+LABEL_SIZE = 26
+TITLE_SIZE = 30
+TICKS_SIZE = 22
+MARK_SIZE = 150
+
+# create a figure and axes object
+fig, ax = plt.subplots(figsize = (WIDTH, HEIGHT))
+
+# read the data into a dataframe and index by PROPERTY_ID
+df = pd.read_csv('multivariate_control_data.csv', index_col = 'PROPERTY_ID', header = 0)
+
+# extract the response variable (percent change from median price)
+Y = df['PERCENTAGE_CHANGE_FROM_MEDIAN_PRICE']
+
+# extract the predictor variable (months post flood, 1-23 and 1-25)
+X = df['MONTHS_POST_FLOOD'].iloc[0:MONTHS]
+
+# grab the response variable for first observation
+avg_Y = np.array(Y.iloc[0:MONTHS])
+
+# define the number of iterations
+iters = int(len(Y) / len(avg_Y))
+
+# compute an element-wise average (grouped by property id) on each observation in the response column
+for idx in range(1, iters):
+    # store the index of the lower bound of the current observation
+    lower = idx*MONTHS
+    # store the index of the upper bound of the current observation
+    upper = (idx + 1)*MONTHS
+    # update the response variable by adding the current observation
+    avg_Y += np.array(Y.iloc[lower:upper])
+
+# scale the sum by the number of observations
+avg_Y /= NUM_OBS
+    
+# do the same thing using a list comprehension
+vals = [np.array(Y.iloc[i*MONTHS : (i + 1)*MONTHS]) for i in range(iters)]
+val_sum = sum(vals)
+
+# scale by the number of observations
+mean_vals = val_sum / NUM_OBS 
+
+
+##############################################################################
+###                          VISUALIZATION                                 ###
+
+# plot average percentage change in the months following Matthew (pink)
+x_matt = X[0:POST_MATT]; y_matt = avg_Y[0:POST_MATT]
+ax.scatter(x_matt, y_matt, label='post-Matthew', s=MARK_SIZE , color="#dd1c77")
+
+# plot average percentage change in the months following Florence (green)
+x_flor = X[POST_MATT:POST_FLOR]; y_flor = avg_Y[POST_MATT:POST_FLOR]
+ax.scatter(x_flor, y_flor, label='post-Florence', s=MARK_SIZE, color="#238b45")
+
+# add features to the plot
+plt.xlabel('Months Post Flood', size=LABEL_SIZE, color='k')
+plt.ylabel('Percentage Change From Median Price', size=LABEL_SIZE, color='k')
+plt.title('Average Percent Change From Median Price vs.\n Months Post Flood (Control Properties)',
+          size=TITLE_SIZE)
+plt.style.use('ggplot')
+plt.legend(facecolor='white', prop={'size': 20})
+plt.xticks(size=TICKS_SIZE)
+plt.yticks(size=TICKS_SIZE)
+plt.show()
---
+++ src/ControlProperties.py
@@ -0,0 +1,88 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct  3 23:29:54 2020
+
+@author: alex raum
+"""
+
+import pandas as pd
+import matplotlib.pyplot as plt
+import matplotlib.ticker as tkr
+
+# define a helper function to help format y-axis
+def yfunc(y, pos):
+    s = '{:0,d}'.format(int(y))
+    return s
+
+# define plot_data function that plots the data in both lists on
+# the same figure in the specified color
+def plot_data(daterange, list1, list2, linecolor, fig_width,
+              fig_height, label_size, title_size, ticks_size, zone):
+    
+    # create a figure and axes object
+    fig, ax = plt.subplots(figsize=(fig_width, fig_height))
+    
+     # set up an axis formatter for thousands seperators    
+    y_format = tkr.FuncFormatter(yfunc)
+    ax.yaxis.set_major_formatter(y_format)
+    
+    # store indices of flood events
+    t1 = daterange[24]
+    t2 = daterange[48]
+    
+    # plot the data
+    ax.plot(daterange, list1, color=linecolor)
+    ax.plot(daterange, list2, color=linecolor)
+    
+    # format the plot
+    ax.set_xlabel('Time (months)', size=label_size, color='k')
+    ax.set_ylabel('Property Value ($)', size=label_size, color='k')
+    ax.set_title('Property Values vs. Time in Zone ' + zone + 
+                 '\n(Unaffected By Flooding)', size=title_size)
+    ax.axvline(x=t1, color='r', linestyle='--', label='Hurricane Matthew')
+    ax.axvline(x=t2, color='k', linestyle='--', label='Hurricane Florence')
+    ax.legend(facecolor="white")
+    plt.xticks(size=ticks_size, rotation=45)
+    plt.yticks(size=ticks_size)
+    plt.style.use('ggplot')
+    plt.show()
+
+
+#####################################################################################
+    
+# define macros
+num_zones = 10
+fig_width = 10
+fig_height = 5
+label_size = 16
+title_size = 22
+ticks_size = 12
+    
+# read in the data and parse by dates (10.2014 - 9.2020)
+df_vals = pd.read_csv('control_property_vals_vs_time.csv', parse_dates=True, index_col='DATE')
+
+# store desired colors in a list
+colors = ["#3182bd", "#fc4e2a", "#006d2c", "#cb181d", "#6a51a3",
+          "#662506", "#dd1c77", "#525252", "#238b45", "#253494"]
+
+# store the daterange of the data
+dates = df_vals.index
+      
+# hold the columns of the dataframe in a list of tuples
+cols = df_vals.columns
+cols_list = list(cols)
+
+# for each of 10 pairs of columns, plot both columns
+# in the pair together on their own plot
+for num in range(num_zones):
+    # store function parameters
+    idx1 = (2 * num)
+    idx2 = (2 * num + 1)
+    vals1 = df_vals[cols_list[idx1]]
+    vals2= df_vals[cols_list[idx2]]
+    color = colors[num]
+    zone = str(num + 1) + 'c'
+    
+    # pass the plot data function both columns in the pair
+    plot_data(dates, vals1, vals2, color, fig_width, fig_height,
+              label_size, title_size, ticks_size, zone)
